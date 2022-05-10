74f82e3adbdd42f1ce1763883aa2e699cfd62f2c
Anibal Montero <anibal.montero.p@gmail.com>
2022-05-10 10:25:55 +0200

    creado los archivos

---
+++ proEleYaAn.js
@@ -0,0 +1,16 @@
+
+let nombre = document.getElementById('nombre');
+  console.log(nombre.value);
+
+function validarNombre() {
+  
+}
+validarNombre();
+function emailconfirmacion() { //esto hay que meterlo en el boton de submit
+
+let email1=document.document.getElementById=('email').value;
+let email2=document.document.getElementById=('emailconfirm').value;
+
+if(email1!=email2){
+  alert('Tus emails no coinciden')
+}
