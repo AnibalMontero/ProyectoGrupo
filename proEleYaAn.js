fa0e82f624ff25dc09540af5414f13f79f5ef662
Anibal Montero <anibal.montero.p@gmail.com>
2022-05-10 14:44:53 +0200

    primera integracion

--- proEleYaAn.js
+++ proEleYaAn.js
@@ -1,5 +1,71 @@
-let nombre = document.getElementById('nombre');
-console.log(nombre.value);
+let nombre = document.querySelector('#nombre');
+let apellido = document.querySelector('#apellido');
+let dni = document.querySelector('#dni');
 
-function validarNombre() {}
-validarNombre();
+nombre.addEventListener('change', () => {
+  let entrada = nombre.value;
+  for (let i = 0; i < entrada.length; i++) {
+    if (
+      (entrada[i].charCodeAt() >= 65 && entrada[i].charCodeAt() <= 90) ||
+      (entrada[i].charCodeAt() >= 97 && entrada[i].charCodeAt() <= 122)
+    ) {
+      priLetra = entrada[0].toUpperCase();
+      resto = entrada.slice(1).toLowerCase();
+      cambio = priLetra + resto;
+      nombre.value = cambio;
+    } else {
+      nombre.value = '';
+    }
+  }
+});
+
+nombre.addEventListener('keypress', () => {
+  let entrada = nombre.value;
+  for (let i = 0; i < entrada.length; i++) {
+    if (
+      (entrada[i].charCodeAt() >= 65 && entrada[i].charCodeAt() <= 90) ||
+      (entrada[i].charCodeAt() >= 97 && entrada[i].charCodeAt() <= 122)
+    ) {
+      priLetra = entrada[0].toUpperCase();
+      resto = entrada.slice(1).toLowerCase();
+      cambio = priLetra + resto;
+      nombre.value = cambio;
+    } else {
+      nombre.value = '';
+    }
+  }
+});
+
+apellido.addEventListener('change', () => {
+  let entrada = apellido.value;
+  for (let i = 0; i < entrada.length; i++) {
+    if (
+      (entrada[i].charCodeAt() >= 65 && entrada[i].charCodeAt() <= 90) ||
+      (entrada[i].charCodeAt() >= 97 && entrada[i].charCodeAt() <= 122)
+    ) {
+      priLetra = entrada[0].toUpperCase();
+      resto = entrada.slice(1).toLowerCase();
+      cambio = priLetra + resto;
+      apellido.value = cambio;
+    } else {
+      apellido.value = '';
+    }
+  }
+});
+
+apellido.addEventListener('keypress', () => {
+  let entrada = apellido.value;
+  for (let i = 0; i < entrada.length; i++) {
+    if (
+      (entrada[i].charCodeAt() >= 65 && entrada[i].charCodeAt() <= 90) ||
+      (entrada[i].charCodeAt() >= 97 && entrada[i].charCodeAt() <= 122)
+    ) {
+      priLetra = entrada[0].toUpperCase();
+      resto = entrada.slice(1).toLowerCase();
+      cambio = priLetra + resto;
+      apellido.value = cambio;
+    } else {
+      apellido.value = '';
+    }
+  }
+});
