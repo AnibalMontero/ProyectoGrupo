95a14a999b3451a1203b01c982952ea71d1b31b0
Anibal Montero <anibal.montero.p@gmail.com>
2022-05-10 10:29:29 +0200

    subir js

--- proEleYaAn.js
+++ proEleYaAn.js
@@ -1,16 +1,5 @@
-
 let nombre = document.getElementById('nombre');
-  console.log(nombre.value);
+console.log(nombre.value);
 
-function validarNombre() {
-  
-}
+function validarNombre() {}
 validarNombre();
-function emailconfirmacion() { //esto hay que meterlo en el boton de submit
-
-let email1=document.document.getElementById=('email').value;
-let email2=document.document.getElementById=('emailconfirm').value;
-
-if(email1!=email2){
-  alert('Tus emails no coinciden')
-}
